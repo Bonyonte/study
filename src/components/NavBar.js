c860a5912d27480950ecff546871719c66e45fb3
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-09-08 21:58:51 +0300

    adding NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App';
+import {Link} from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+        <Link to='/'>Home</Link>
+        <Link to='/library'>News</Link>
+        <Link to='/comment'>Comment</Link>
+    </div>
+  );
+};
+
+export default NavBar
